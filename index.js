28f5ac710b9f3278f30917bc5d1f52941e5e85e2
nujabec <nujabec@gmail.com>
2023-12-19 08:09:25 +0900

    1st commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const { createProxyMiddleware} = require("http-proxy-middleware")
+const rateLimit = require("express-rate-limit")
+require("dotenv").config()
+
+const limiter = rateLimit({
+    windowMS: 15 * 60 * 1000, // 15 minutes
+    max: 10000,
+})
+
+app.get("/", (req, res) => {
+    res.send("This is my proxy server")
+})
+
+// 全てのサイトに適用する場合
+// app.use(limiter)
+
+app.use("/corona-tracker-world-data", limiter, (req, res, next) => {
+    createProxyMiddleware({
+        target: process.env.BASE_API_URL_CORONA_WORLD,
+        changeOrigin: true,
+        pathRewrite: {
+            [`^/corona-tracker-world-data`]:"",
+        },
+    })(req, res, next)
+})
+
+
+const port = process.env.PORT || 5000
+
+app.listen(5000, () => {
+    console.log(`Listening on port ${port}`)
+})
+
+module.exports = app
